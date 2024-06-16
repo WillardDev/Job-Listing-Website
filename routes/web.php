3c2da84ac98f8b6af77f09f4007854a9681b2cec
WillardDev <willex.ox@gmail.com>
2024-06-16 19:44:07 +0300

    changes on web.php

--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
-use App\Models\Job;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
 
-// Index
 Route::get('/jobs', [JobController::class, 'index']);
-
-// create
 Route::get('/jobs/create', [JobController::class, 'create']);
-
-// Show
 Route::get('/jobs/{job}', [JobController::class, 'show']);
-
-// Store
 Route::post('/jobs', [JobController::class, 'store']);
-
-// Edit
 Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-
-// Update
 Route::patch('/jobs/{job}', [JobController::class, 'update']);
-
-// Destroy
 Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/contact', 'contact');
