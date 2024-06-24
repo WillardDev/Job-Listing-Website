47c59cfe11575435f6148417b319b31bdcde6379
WillardDev <willex.ox@gmail.com>
2024-06-24 12:32:25 +0300

    addition on registration

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\JobController;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
